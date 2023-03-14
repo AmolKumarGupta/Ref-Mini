69e0b4cbf3da4cebf718f24e1d26d190cd4d707b
amol <amlo1232001@gmail.com>
2023-03-14 23:17:38 +0530

    fix #16, remove green div on loggedin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,12 +48,12 @@
     @yield('guest')
   @endguest
 
-  @if(session()->has('success'))
+  @if(false && session()->has('success'))
     <div x-data="{ show: true}"
         x-init="setTimeout(() => show = false, 4000)"
         x-show="show"
         class="position-fixed bg-success rounded right-3 text-sm py-2 px-4">
-      <p class="m-0">{{ session('success')}}</p>
+      <p class="m-0">{{ session('success') }}</p>
     </div>
   @endif
     <!--   Core JS Files   -->
