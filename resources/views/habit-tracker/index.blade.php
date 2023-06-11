c28986f96e0737095e90e336ae30862854630db2
amol <amlo1232001@gmail.com>
2023-06-12 00:43:16 +0530

    add habit tracker blade file

--- resources/views/habit-tracker/index.blade.php
+++ resources/views/habit-tracker/index.blade.php
@@ -3,7 +3,16 @@
 @section('content')
 <div class="container-fluid py-4">
     <div class="row gap-3 gap-lg-0">
+        <div class="card">
 
+            <div class="card-header pb-0">
+                <h5>Dashboard for Tracker</h5>
+            </div>
+
+            <div class="card-body">
+            </div>
+
+        </div>
     </div>
 </div>
 
