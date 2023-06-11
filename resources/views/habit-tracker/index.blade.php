8371847a98fc2155f4340da4188d278509ed6203
amol <amlo1232001@gmail.com>
2023-06-12 01:27:40 +0530

    feat: (#26) Page Template

--- resources/views/habit-tracker/index.blade.php
+++ resources/views/habit-tracker/index.blade.php
@@ -10,6 +10,43 @@
             </div>
 
             <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table" id="track">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Categories</th>
+                                <th>Time</th>
+                                <th>Created At</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <th>Exercise</th>
+                                <th>exercise</th>
+                                <th>15 mins</th>
+                                <th>10 May, 10:00am</th>
+                            </tr>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Categories</th>
+                                <th>Time</th>
+                                <th>Created At</th>
+                            </tr>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Categories</th>
+                                <th>Time</th>
+                                <th>Created At</th>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
         </div>
@@ -20,4 +57,9 @@
 @endsection
 
 @section('scripts')
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        let table = new simpleDatatables.DataTable('#track');
+    });
+</script>
 @endsection
