01f68431f1a2bbd2d6c03fad8ee62d295d9d96f7
amol <amlo1232001@gmail.com>
2023-06-14 02:10:29 +0530

    feat: (#28) add tracks

--- resources/views/habit-tracker/index.blade.php
+++ resources/views/habit-tracker/index.blade.php
@@ -8,7 +8,7 @@
             <div class="card-header pb-0">
                 <h5>
                     Dashboard for Tracker
-                    <div data-bs-toggle="modal" data-bs-target="#modal-track-create" role="button" class="icon icon-shape icon-xxs shadow border-radius-sm bg-gradient-info text-center" style="line-height: 1;">
+                    <div data-bs-toggle="modal" data-bs-target="#modal-track" role="button" class="icon icon-shape icon-xxs shadow border-radius-sm bg-gradient-info text-center" style="line-height: 1;">
                         <i class="fa fa-xs fa-plus" style="top: -25%" aria-hidden="true"></i>
                     </div>
                 </h5>
@@ -59,58 +59,20 @@
     </div>
 </div>
 
-<div class="modal fade" id="modal-track-create" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
-    <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h6 class="modal-title" id="modal-title-default">Create Track</h6>
-                <button type="button" class="btn-close text-dark" data-bs-dismiss="modal" aria-label="Close">
-                    <i class="fa fa-times"></i>
-                </button>
-            </div>
-
-            <form onsubmit="createItem(event)">
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label for="item_name">Name</label>
-                                <input type="text" name="name" class="form-control" placeholder="Name" id="item_name">
-                                <small data-error="name" class="text-danger"></small>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="item_url">Url</label>
-                                <input type="text" name="url" class="form-control" placeholder="Ex. menu" id="item_url">
-                                <small data-error="url" class="text-danger"></small>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="item_icon">Icon</label>
-                                <div class="input-group">
-                                    <span class="input-group-text"><i class="fa fa-home"></i></span>
-                                    <input type="text" name="icon" class="form-control" placeholder="Fontawesome icon" id="item_icon" onkeyup="setFormIconDisplay(this)">
-                                </div>
-                                <small data-error="icon" class="text-danger"></small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn bg-gradient-info">Save changes</button>
-                    <button type="button" class="btn ml-auto" data-bs-dismiss="modal">Close</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
+@livewire('habit-track.modals.track')
 
 @endsection
 
 @section('scripts')
 <script>
+    let trackModal = new bootstrap.Modal('#modal-track');
+
     document.addEventListener('DOMContentLoaded', function() {
         let table = new simpleDatatables.DataTable('#track');
     });
+
+    window.livewire.on('closeModal', function() {
+        trackModal.hide();
+    })
 </script>
 @endsection
---
+++ resources/views/livewire/habit-track/modals/track.blade.php
@@ -0,0 +1,49 @@
+<div class="modal fade @if($modalOpen) show @endif" @if($modalOpen) style="display: block;" @endif id="modal-track" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
+    <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title" id="modal-title-default">Create Track</h6>
+                <button type="button" class="btn-close text-dark" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="fa fa-times"></i>
+                </button>
+            </div>
+
+            <form wire:submit.prevent="save">
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="item_name">Name</label>
+                                <input wire:model.defer="formdata.name" type="text" name="name" class="form-control" placeholder="Name">
+                                @error('formdata.name') <small data-error="name" class="text-danger">{{ $message }}</small> @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="description">Description</label>
+                                <input wire:model.defer="formdata.description" type="text" name="description" class="form-control" placeholder="">
+                                @error('formdata.description') <small data-error="url" class="text-danger">{{ $message }}</small> @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="time">Time</label>
+                                <input wire:model.defer="formdata.time" type="number" name="time" class="form-control" placeholder="">
+                                @error('formdata.time') <small data-error="url" class="text-danger">{{ $message }}</small> @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="date">Date</label>
+                                <input wire:model.defer="formdata.date" type="date" name="date" class="form-control" placeholder="">
+                                @error('formdata.date') <small data-error="url" class="text-danger">{{ $message }}</small> @enderror
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn bg-gradient-info">Save changes</button>
+                    <button type="button" class="btn ml-auto" data-bs-dismiss="modal">Close</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
