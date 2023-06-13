c93694decb9a5eafbb252c63fbf89072e2bf7522
amol <amlo1232001@gmail.com>
2023-06-14 00:34:37 +0530

    migration

---
+++ app/Models/HabitTrack.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HabitTrack extends Model
+{
+    use HasFactory;
+}
