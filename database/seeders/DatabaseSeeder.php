a61c526957b8fda87a5e5cd6c83af63a46a1e53b
amol <amlo1232001@gmail.com>
2023-08-06 00:31:09 +0530

    feat: composer run lint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             MenuSeeder::class,
-            CategorySeeder::class
+            CategorySeeder::class,
         ]);
     }
 }
